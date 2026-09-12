cbbdefe3f46db30ef263bc2a84db18929ae76591
Arnav Agrawal <arnavagr@usc.edu>
2026-09-12 18:25:26 -0400

    Add: design-word-search-data-structure - submission-1

---
+++ Data Structures & Algorithms/design-word-search-data-structure/submission-1.py
@@ -0,0 +1,41 @@
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.endOfWord = False
+    
+
+class WordDictionary:
+
+    def __init__(self):
+        self.root = TrieNode()
+        
+
+    def addWord(self, word: str) -> None:
+        cur = self.root
+        for c in word:
+            if c not in cur.children:
+                cur.children[c] = TrieNode()
+            cur = cur.children[c]
+        
+        cur.endOfWord = True
+        
+
+    def search(self, word: str) -> bool:
+        def dfs(i, cur):
+            if i == len(word):
+                return cur.endOfWord
+            
+            ch = word[i]
+
+            if ch == '.':
+                for child in cur.children.values():
+                    if dfs(i + 1, child):
+                        return True
+                return False
+            
+            if ch not in cur.children:
+                return False
+            
+            return dfs(i + 1, cur.children[ch])
+
+        return dfs(0, self.root)
