373258c2ef92ed5a2af153c9949636b7c893068c
Arnav Agrawal <arnavagr@usc.edu>
2026-09-15 15:37:18 -0400

    Add: house-robber - submission-4

---
+++ Data Structures & Algorithms/house-robber/submission-4.py
@@ -0,0 +1,12 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        mem = [0] * (len(nums) + 1)
+
+        mem[len(nums) - 1] = nums[-1]
+
+        print(mem)
+
+        for i in range(len(nums) - 2, -1, -1):
+            mem[i] = max(nums[i] + mem[i + 2], mem[i + 1])
+        
+        return mem[0]
